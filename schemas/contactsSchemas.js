86e3ee2515d61fe48819efc4f56b52566f22144e
MrRoik <138239220+MrRoik@users.noreply.github.com>
2024-04-08 14:34:28 +0300

    create hw-03

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -2,13 +2,11 @@ import Joi from "joi";
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
-  email: Joi.string()
-    .email({
-      minDomainSegments: 2,
-      tlds: { allow: ["com", "net", "ua"] },
-    })
-    .required(),
-  phone: Joi.string().min(7).max(12).required(),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net", "ua"] },
+  }),
+  phone: Joi.string().min(7).max(12),
 });
 
 export const updateContactSchema = Joi.object({
@@ -21,3 +19,7 @@ export const updateContactSchema = Joi.object({
 })
   .min(1)
   .message("Body must have at least one field");
+
+export const updateStatusContactSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
