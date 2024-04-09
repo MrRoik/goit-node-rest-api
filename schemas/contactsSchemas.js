f0d535b606d167a97abc071a315c137b22b951b5
MrRoik <138239220+MrRoik@users.noreply.github.com>
2024-04-09 10:37:51 +0300

    update contactsSchemas

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -2,11 +2,13 @@ import Joi from "joi";
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email({
-    minDomainSegments: 2,
-    tlds: { allow: ["com", "net", "ua"] },
-  }),
-  phone: Joi.string().min(7).max(12),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net", "ua"] },
+    })
+    .required(),
+  phone: Joi.string().min(7).max(12).required(),
 });
 
 export const updateContactSchema = Joi.object({
