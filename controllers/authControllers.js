0d51aaf183a0117cc98607e281d597f115f652bb
MrRoik <138239220+MrRoik@users.noreply.github.com>
2024-04-22 17:33:03 +0300

    update rep

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,7 +1,12 @@
 import HttpError from "../helpers/HttpError.js";
 import bcrypt from "bcrypt";
+import crypto from "crypto";
+import path from "path";
+import fs from "fs/promises";
 import { User } from "../models/userModals.js";
 import { registerToken } from "../services/jwtServices.js";
+import Jimp from "jimp";
+import { v4 } from "uuid";
 
 export const registerUser = async (req, res, next) => {
   const { email, password, subscription } = req.body;
@@ -12,6 +17,9 @@ export const registerUser = async (req, res, next) => {
       throw HttpError(409, "Email in use");
     }
 
+    const emailHash = crypto.createHash("md5").update(email).digest("hex");
+    const avatarURL = `https:gravatar.com/avatar/${emailHash}.jpg?d=robohash`;
+
     const salt = await bcrypt.genSalt(10);
     const passwordHash = await bcrypt.hash(password, salt);
 
@@ -19,6 +27,7 @@ export const registerUser = async (req, res, next) => {
       email,
       password: passwordHash,
       subscription,
+      avatarURL,
     });
 
     res.status(201).json({
@@ -80,6 +89,37 @@ export const logoutUser = async (req, res, next) => {
     res.status(204).json({
       message: "Not authorized",
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const uploadUserAvatar = async (req, res, next) => {
+  const { id } = req.user;
+
+  try {
+    if (!req.file) {
+      throw HttpError(400, "File not uploaded");
+    }
+
+    const { path: tmpUpload, originalname } = req.file;
+
+    const filename = `${id}-${v4()}-${originalname}`;
+
+    const changeSizeAvatar = await Jimp.read(tmpUpload);
+    changeSizeAvatar.resize(250, 250).write(tmpUpload);
+
+    await fs.rename(tmpUpload, path.join("public/avatars", filename));
+
+    const updateAvatarURL = path.join("/avatars", filename);
+
+    const user = await User.findByIdAndUpdate(
+      id,
+      { updateAvatarURL },
+      { new: true }
+    );
+
+    res.status(200).json({ avatarURL: user.avatarURL });
   } catch (error) {
     next(error);
   }
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -6,8 +6,10 @@ import {
   loginUser,
   registerUser,
   logoutUser,
+  uploadUserAvatar,
 } from "../controllers/authControllers.js";
 import { protect } from "../middlewares/authMiddlewares.js";
+import { uploadAvatar } from "../middlewares/userMiddlewares.js";
 
 const authRouter = express.Router();
 
@@ -15,5 +17,6 @@ authRouter.post("/register", validateBody(registerSchema), registerUser);
 authRouter.post("/login", validateBody(loginSchema), loginUser);
 authRouter.post("/logout", protect, logoutUser);
 authRouter.get("/current", protect, getCurrentUser);
+authRouter.patch("/avatars", protect, uploadAvatar, uploadUserAvatar);
 
 export default authRouter;
